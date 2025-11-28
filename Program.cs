dce1f1eb8503c0cb7967850a48c29a4e209b6b9e
Emma <emma@Emmas-MacBook-Air.local>
2025-11-28 12:04:04 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using NEA.Components;
+using Blazored.Modal;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddBlazoredModal();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.MapStaticAssets();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
