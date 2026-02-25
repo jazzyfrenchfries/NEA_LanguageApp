44125dfc81246b5da271ce651084fd48098400ef
Emma Kemp <emmalkemp@outlook.com>
2026-02-25 10:40:40 +0000

    Added input validation for new users on their username and password pt2

--- Components/Pages/RegisterModel.cs
+++ Components/Pages/RegisterModel.cs
@@ -5,6 +5,5 @@ using System.ComponentModel.DataAnnotations;
         public string Username {get; set;}
         [Required(ErrorMessage ="Password is required")]
         [MinLength(8, ErrorMessage ="Password must be at least 8 characters")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "Password must contain at least 1 uppercase, 1 lowercase and 1 number ")]
         public string Password { get; set;}
     }
